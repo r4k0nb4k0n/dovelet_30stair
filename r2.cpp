9c01453d6e07327cbab8adae718a692c3de6063d
r4k0nb4k0n <chj6097@gmail.com>
2016-09-27 21:39:37 +0900

    Create r2.cpp

---
+++ r2.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int r1, m;
+     
+    scanf("%d %d",&r1,&m);
+     
+    int r2 = 2*m-r1;
+     
+    printf("%d",r2);
+     
+    return 0;
+}
