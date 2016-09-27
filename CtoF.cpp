e391e5214648ce14c42b9a0a0352fc5733bfbf41
r4k0nb4k0n <chj6097@gmail.com>
2016-09-27 21:33:07 +0900

    Create CtoF.cpp

---
+++ CtoF.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int C;
+    scanf("%d",&C);
+    double F;
+    F=((double)(C*9)/(double)5)+(double)32;
+    printf("%.1lf",F);
+    return 0;
+}
