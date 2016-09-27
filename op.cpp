0fd4573d422da2a18fff2205ccf3581785ca7f1a
r4k0nb4k0n <chj6097@gmail.com>
2016-09-27 21:30:57 +0900

    Create op.cpp

---
+++ op.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+int main()
+{
+  int a, b;
+  scanf("%d %d",&a,&b);
+  printf("%d+%d=%d\n",a,b,a+b);
+  printf("%d-%d=%d\n",a,b,a-b);
+  printf("%d*%d=%d\n",a,b,a*b);
+  printf("%d/%d=%d\n",a,b,a/b);
+  printf("%d%%%d=%d",a,b,a%b); 
+  return 0;
+}
